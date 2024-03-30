68a312caa448326c7383b820d6e21ca1a8efb217
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-03-30 16:12:58 -0300

    mundaça

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Simulador_processos"
+  base: "/Simulador_processos",
+  plugins: [react()]
 })
